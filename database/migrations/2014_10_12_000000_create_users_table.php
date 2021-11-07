2c73e1539ebc8dcba74822803b0256a8c0f59510
Hesham Yahia <hesham14yahia@gmail.com>
2021-11-08 00:18:07 +0200

    seed super admin user with user migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,6 +3,8 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 class CreateUsersTable extends Migration
 {
@@ -22,6 +24,12 @@ class CreateUsersTable extends Migration
             $table->rememberToken();
             $table->timestamps();
         });
+
+        User::create([
+            "name" => "Super Admin",
+            "email" => "super_admin@admin.com",
+            "password" => Hash::make('password')
+        ]);
     }
 
     /**
